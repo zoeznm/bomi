1d058b3698e88ab6c70aca2ad2c5f373e1f91a37
matomabo <>
2024-05-31 13:10:28 +0900

    am end

--- scope.js
+++ scope.js
@@ -333,3 +333,19 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   createStars();
 });
+
+// 별똥별
+document.addEventListener('DOMContentLoaded', () => {
+  const numStars = 100;
+  const container = document.querySelector('.shooting-stars');
+
+  for (let i = 0; i < numStars; i++) {
+      const star = document.createElement('div');
+      star.className = 'star';
+      star.style.top = `${Math.random() * 100}vh`;
+      star.style.left = `${Math.random() * 100}vw`;
+      star.style.animationDelay = `${Math.random() * 5}s`;
+      star.style.animationDuration = `${2 + Math.random() * 3}s`;
+      container.appendChild(star);
+  }
+});
